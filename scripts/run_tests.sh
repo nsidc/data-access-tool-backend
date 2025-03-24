b91d00e73c3e5ff5b978bb6fd8028730c654903d
Trey Stafford <trey.stafford@colorado.edu>
2025-03-24 15:11:59 -0600

    Fixup `run_tests.sh`

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker compose -f docker-compose-test.yml build 
-docker compose -f docker-compose-test.yml run --rm downloader-script
+docker compose -f docker-compose-test.yml run --rm api
