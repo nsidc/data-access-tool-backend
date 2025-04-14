6794a91ecdbfdbbd3b3bce5bc5ec4b59291d34d9
Trey Stafford <trey.stafford@colorado.edu>
2025-04-14 11:05:53 -0600

    Add unit test for metrics endpoint
    Ensure HTML is generated and returned with expected text

--- test/unit/test_metrics.py
+++ test/unit/test_metrics.py
@@ -1,15 +1,29 @@
 from pathlib import Path
 
-from dat_backend.routes.metrics import metrics_from_logs
+from dat_backend.routes import metrics
+from dat_backend import app
+
+
+app.testing = True
+
+
+TEST_METRICS_PATH = Path(__file__).parent / "test_server_logs"
 
 
 def test_metrics_from_logs():
-    test_metrics_path = Path(__file__).parent / "test_server_logs"
-    metrics = metrics_from_logs(test_metrics_path)
+    metrics_dict = metrics.metrics_from_logs(TEST_METRICS_PATH)
 
-    assert metrics.get("max_datetime") is not None
-    assert metrics.get("min_datetime") is not None
+    assert metrics_dict.get("max_datetime") is not None
+    assert metrics_dict.get("min_datetime") is not None
 
-    assert metrics.get("total_num_requests") == 7
-    assert metrics["get_links_metrics"]["ATL06_6"]["200"] == 2
-    assert metrics["uri_specific_metrics"]["/api/status"]["count"]["200"] == 4
+    assert metrics_dict.get("total_num_requests") == 7
+    assert metrics_dict["get_links_metrics"]["ATL06_6"]["200"] == 2
+    assert metrics_dict["uri_specific_metrics"]["/api/status"]["count"]["200"] == 4
+
+
+def test_metrics_endpoint(monkeypatch):
+    monkeypatch.setattr(metrics, "SERVER_LOGS_DIR", TEST_METRICS_PATH)
+    with app.test_client() as client:
+        result = client.get("/api/metrics")
+        assert result.status_code == 200
+        assert "Total number of requests handled: 7" in result.text
