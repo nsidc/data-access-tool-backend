6d992a949764018b71d10f539c24ee51b0f3851c
Trey Stafford <trey.stafford@colorado.edu>
2026-09-21 17:21:09 -0600

    Update `test_get_links` for ATL06 v7

--- test/unit/test_get_links.py
+++ test/unit/test_get_links.py
@@ -23,7 +23,7 @@ def _get_links_response(cursor: str | None = None):
         # cmr request params. Eventually we may want to extract this as a
         # separate query param that overrides whatever is given by the
         # cmr_query_params.
-        cmr_request_params = "provider=NSIDC_CPRD&page_size=5&sort_key[]=-start_date&sort_key[]=producer_granule_id&short_name=ATL06&version=6&version=06&version=006&temporal[]=2018-10-14T00:00:00Z,2025-02-25T00:25:20Z&bounding_box=-180,-90,180,90&options[producer_granule_id][pattern]=true&producer_granule_id[]=*ATL06_2024*_0804*_006_01.h5*"
+        cmr_request_params = "provider=NSIDC_CPRD&page_size=5&sort_key[]=-start_date&sort_key[]=producer_granule_id&short_name=ATL06&version=6&version=07&version=007&temporal[]=2018-10-14T00:00:00Z,2025-02-25T00:25:20Z&bounding_box=-180,-90,180,90&options[producer_granule_id][pattern]=true&producer_granule_id[]=*ATL06_2024*_0804*_007_01.h5*"
         url_encoded_cmr_request_params = quote(cmr_request_params)
         cursor_query_param = ""
         if cursor:
@@ -62,10 +62,10 @@ def test_get_links():
     assert len(data_links) == 5
     assert result.json["done"] is False
 
-    for idx in range(1, 7 + 1):
+    for idx in range(1, 6 + 1):
         if result.json["done"]:
-            # 31 ganules, 5 granules per page.
-            raise RuntimeError(f"Expected only 7 pages of results. Done after {idx}")
+            # 30 ganules, 5 granules per page.
+            raise RuntimeError(f"Expected only 6 pages of results. Done after {idx}")
 
         result = _get_links_response(result.json["cursor"])
         data_links.extend(
@@ -78,4 +78,4 @@ def test_get_links():
     assert len(data_links) == len(set(data_links))
 
     # We expect a total of 31 granule results for the query.
-    assert len(data_links) == 31
+    assert len(data_links) == 30
